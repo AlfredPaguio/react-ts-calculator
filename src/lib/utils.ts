b510a436f62b8cadb79f07e819e34c8b7a7fec29
AlfredPaguio <alfredpaguio36@gmail.com>
2024-01-12 03:28:19 +0800

    Prevent rounding off if the number is so big

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,14 +10,27 @@ const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
   maximumFractionDigits: 0,
 });
 
-export function formatOperand(operand: string) {
+export function formatOperand(operand: string): string | undefined {
   if (operand == null) return;
+
+  // this is for the final result
+  // Check if the result is in scientific notation
+  // https://www.codeproject.com/Questions/1075617/using-regular-expression-to-check-an-string-if-its
+  // https://regexper.com/#%5Cd*%28%5C.%5Cd*%29%3F%5BeE%5D%5Cd*
+  if (/\d*(\.\d*)?[eE]\d*/.test(operand)) {
+    // Convert the scientific notation to a string with high precision, I don't need to split it because it's the result
+    return Number(operand).toLocaleString("en-US", {
+      maximumFractionDigits: 20,
+    });
+  }
+
   //split the left and right side of decimal
   const [integer, decimal] = operand.split(".");
   if (decimal == null) {
     //format the "number" immediately if there's no decimal
-    return INTEGER_FORMATTER.format(+integer);
+    console.log(INTEGER_FORMATTER.format(BigInt(integer)));
+    return INTEGER_FORMATTER.format(BigInt(integer));
   }
   //format the number on the left side and append the decimal
-  return `${INTEGER_FORMATTER.format(+integer)}.${decimal}`;
+  return `${INTEGER_FORMATTER.format(BigInt(integer))}.${decimal}`;
 }
